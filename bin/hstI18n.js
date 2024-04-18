258eba335a16003cb5f563f0ccb4c94a1b407408
mya.gong <mya.gong@hstong.com>
2024-04-18 20:35:33 +0800

    feat: 完善功能

--- bin/hstI18n.js
+++ bin/hstI18n.js
@@ -11,41 +11,71 @@ const questions = [
     message: '请选择操作类型？',
     name: 'type',
     choices: [
+        // {
+        //     name: '生成翻译文件i18n.xlsx（只支持英文、中文）',
+        //     value: 'export'
+        // },
+        // {
+        //     name: '写入翻译i18n文件夹',
+        //     value: 'import'
+        // },
         {
-            name: '生成翻译文件i18n.xlsx（只支持英文、中文）',
-            value: 'export'
-        },
-        {
-            name: '写入翻译i18n文件夹',
-            value: 'import'
-        },
-        {
-          name: '导出yaml翻译文件',
+          name: '把代码中 Yaml 文件导出成 Excel 翻译文件',
           value: 'exportY'
         },
         {
-          name: '导入yaml翻译文件',
+          name: '把代码中 Excel 文件（i18n.xlsx）转成 yaml 文件',
           value: 'importY'
         }
     ]
+  },
+  {
+    type: 'checkbox',
+    name: 'langs',
+    message: '请选择导出的语种（空格选中）？',
+    choices: ['zh_CN', 'en_US', 'zh_HK', 'my_MS'],
+    default: ['zh_CN', 'en_US', 'zh_HK'],
+    when: (answers) => answers.type === 'exportY'
+  },
+  {
+    type: 'list',
+    name: 'isTemplate',
+    message: '是否存在旧的 Excel 翻译模板（demo.xlsx），自动匹配在一起',
+    choices: [
+      { name: '是', value: '1' },
+      { name: '否', value: '2' },
+    ],
+    when: (answers) => answers.type === 'exportY'
+  },
+  {
+    type: 'list',
+    name: 'defaultLang',
+    message: '默认语言',
+    choices: [
+      { name: 'zh_CN', value: 'zh_CN' },
+      { name: 'en_US', value: 'en_US' },
+      { name: 'zh_HK', value: 'zh_HK' },
+      { name: 'my_MS', value: 'my_MS' },
+    ],
+    default: 'zh_CN',
   },
 ]
 
 inquirer.prompt(questions).then(answer=>{
   console.log(answer);
-  const {type} = answer;
+  const {type, langs, isTemplate, defaultLang = 'zh_CN'} = answer;
   switch (type) {
-    case 'export':
-      const headColumns = ['code', 'zh_CN', 'en_US']
-      exportExcel(headColumns);
-      break;
-    case 'import':
-      const headColumns1 = ['zh_CN', 'en_US', 'zh_HK']
-      analysisExcel(headColumns1);
-      break;
+    // case 'export':
+    //   const headColumns = ['code', 'zh_CN', 'en_US']
+    //   exportExcel(headColumns);
+    //   break;
+    // case 'import':
+    //   const headColumns1 = ['zh_CN', 'en_US', 'zh_HK']
+    //   analysisExcel(headColumns1);
+    //   break;
     case 'exportY':
-      const headColumns2 = ['code', 'zh_CN', 'en_US', 'zh_HK'];
-      toExcel(headColumns2);
+      const headColumns2 = ['code', ...langs];
+      toExcel(headColumns2, langs, isTemplate, defaultLang);
       break;
     case 'importY':
       const headColumns3 = ['zh_CN', 'en_US', 'zh_HK'];
--- bin/translateYaml/toExcel.js
+++ bin/translateYaml/toExcel.js
@@ -1,63 +1,93 @@
-const fs = require("fs");
 const glob = require("glob");
 const path = require("path");
-const { writeFile } = require('../../utils/generateExcel');
+const { writeFile, readFile } = require('../../utils/generateExcel');
 const { assemblyData, getJsonDataByYaml } = require('../../utils/common');
 
 const data = [];
+const demoData = {};
 const cwd = process.cwd(); 
+const excelPath = path.join(cwd, 'demo.xlsx'); // 文件目录
 const projectPath = path.join(cwd, ''); // 前端项目目录
 let localesLang = {
   zh_CN: {},
-  en_US: {}
+  en_US: {},
+  zh_HK: {},
+  my_MS: {},
 };
 
-module.exports = async (columns) => {
+module.exports = async (columns, langs, isTemplate, defaultLang) => {
   /**
-   * 1、读取yaml文件
-   * 2、把yaml文件转化为扁平化json对象
+   * 1、读取yaml文件,把yaml文件转化为扁平化json对象
+   * 2、读取模板文件（把模板文件里面的翻译）
    * 3、生成excel文件
    */
 
-  // 1、读取yaml文件
+  // 1、读取yaml文件,把yaml文件转化为扁平化json对象
   const readYamlFile = async () => {
+    const matchLang = (name, lang) => {
+      const langCopy = lang.replace('_', '-');
+      return name.includes(lang) || name.includes(langCopy);
+    }
+
     const localeFiles = glob.sync("**/*.+(yaml)", {
       cwd: projectPath,
       ignore: "**/node_modules/**", // 忽略node_modules文件夹
     });
+
     for (const localeFile of localeFiles) {
       // 读取英文翻译
-      if (localeFile.includes('en_US') || localeFile.includes('en-US') ) {
-        localesLang['en_US']  = {
-          ...localesLang['en_US'],
-          ...await getJsonDataByYaml(localeFile),
-        };
-      }
-      // 读取中文翻译
-      if (localeFile.includes('zh_CN') || localeFile.includes('zh-CN') ) {
-        localesLang['zh_CN']  = {
-          ...localesLang['zh_CN'],
-          ...await getJsonDataByYaml(localeFile),
-        };
+      for (const lang of langs) {
+        const f = matchLang(localeFile, lang);
+        if (f) {
+          localesLang[lang]  = {
+            ...localesLang[lang],
+            ...await getJsonDataByYaml(localeFile),
+          };
+        }
       }
     }
   }
 
   await readYamlFile();
-  
 
-  // 2、把yaml文件转化为扁平化json对象
-  const yamlToJson = async () => {
-    Object.keys(localesLang['zh_CN']).forEach((res) => {
+  // 2、读取模板文件（把模板文件里面的翻译）
+  const formateData = () => {
+    if (isTemplate === '2') {
+      return
+    }
+
+    const dd = readFile({
+      path: excelPath
+    }) || {};
+
+    dd.forEach((res) => {
+      const { code, ...other } = res
+      demoData[code?.toLowerCase()] = other;
+    })
+  }
+
+  formateData();
+  
+  const toJson = async () => {
+    const formateJson = (code) => {
+      const item = {};
+      const key = localesLang[defaultLang][code] || '';
+      const obj = demoData[key?.toLowerCase()] || {};
+      langs.forEach((res) => {
+        item[res] = localesLang[res][code] || obj[res] || '';
+      })
+      return item;
+    }
+
+    Object.keys(localesLang[defaultLang]).forEach((code) => {
       data.push({
-        code: res,  // 唯一编码
-        zh_CN: localesLang['zh_CN'][res], // 中文
-        en_US: localesLang['en_US'][res], // 英文
+        code,
+        ...formateJson(code)
       })
     })
   }
 
-  yamlToJson();
+  toJson();
   
   // 3、生成excel文件
   const newData = assemblyData(data, columns);
